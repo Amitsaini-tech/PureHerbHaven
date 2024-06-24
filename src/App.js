25c9799eaca3af1cf9282442078329e8a5fd8674
Amitsaini-tech <sainiamit3464@gmail.com>
2024-06-24 17:41:28 +0530

    new maincontainer.jsx create or initiziled in App.js using react router dom method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import MainContainer from "./Components/MainContainer"
+import { Route, Routes } from 'react-router-dom'
 const App = () => {
   return (
-    <div>App</div>
+    <div className="w-screen h-full flex flex-col">
+      <main>
+        <Routes>
+          <Route path='/*' element={<MainContainer />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
