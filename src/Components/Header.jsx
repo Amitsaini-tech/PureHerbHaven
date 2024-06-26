5ade467c382edd6839cb6fea0276f521162a46ac
Amitsaini-tech <sainiamit3464@gmail.com>
2024-06-26 13:17:41 +0530

    added user or search icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Instagram from "../image/instagram.png"
 import Facebook from "../image/facebook.png"
 import Whatsapp from "../image/whatsapp.png"
+import { IoIosSearch } from "react-icons/io";
+import user from "../image/user.png"
 
 const Header = () => {
   return (
@@ -11,7 +13,7 @@ const Header = () => {
         <div className="flex flex-row items-center justify-center bg-gray-600 w-[24rem] h-[7rem] text-gray-100 text-[2.3rem] font-semibold"
           style={{
             fontFamily: "Playwrite AU SA,cursive"
-          }}><span className="-rotate-2">PureHerbHaven</span> 
+          }}><span className="-rotate-2">PureHerbHaven</span>
         </div>
         <div className=" flex flex-col justify-center  bg-red-500 w-full">
           <div className="flex flex-row items-center justify-between bg-pink-500 w-full h-[3.5rem]">
@@ -31,7 +33,7 @@ const Header = () => {
               </ul>
             </div>
           </div>
-{/* second */}
+          {/* second */}
           <div className="flex flex-row items-center justify-between bg-pink-900 w-full h-[3.5rem]">
             <div >
               <ul className="flex flex-row items-center justify-center">
@@ -42,8 +44,13 @@ const Header = () => {
                 <li className=" flex items-center justify-center text-gray-100 font-sans font-medium hover:text-green-300 cursor-pointer   h-10 mx-2">Help & support</li>
               </ul>
             </div>
-            <div>
-              
+            <div className="flex flex-row ">
+            <p className=" flex items-center justify-center w-10 h-10 mx-2 bg-white rounded-full">
+          
+              <IoIosSearch className=" w-10 h-6" />
+              </p>
+              <p className=" flex items-center justify-center w-10 h-10 mx-2 rounded-full">
+                <img src={user} alt="" /></p>
             </div>
           </div>
 
@@ -56,5 +63,7 @@ const Header = () => {
     </div >
   )
 }
+
+
 
 export default Header
