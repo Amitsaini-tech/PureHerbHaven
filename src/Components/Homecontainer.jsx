e6c4ef19794830fabb7d596c2f84257ca3a9df82
Amitsaini-tech <sainiamit3464@gmail.com>
2024-07-02 15:04:42 +0530

    clear error

--- src/Components/Homecontainer.jsx
+++ src/Components/Homecontainer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Midcontainer from "./midContainer";
+import Midcontainer from "./MidContainer";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import sandalwood from "../image/sandalwood.avif"
