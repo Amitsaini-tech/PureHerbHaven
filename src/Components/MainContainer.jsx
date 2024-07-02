224248618af90c7b7ec6d0f31421eaa36af0eb2e
Amitsaini-tech <sainiamit3464@gmail.com>
2024-07-02 15:06:48 +0530

    category file added

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Homecontainer from './Homecontainer'
+import Category from './category'
 
 const MainContainer = () => {
   return (
     <div className="">
       <Homecontainer/>
+      <Category/>
     </div>
   )
 }
