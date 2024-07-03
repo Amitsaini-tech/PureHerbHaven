abe123cecb937dae91b9d3073306965e2570145e
Amitsaini-tech <sainiamit3464@gmail.com>
2024-07-03 15:45:07 +0530

    hover add

--- src/Components/category.jsx
+++ src/Components/category.jsx
@@ -4,25 +4,26 @@ import { motion } from "framer-motion"
 const Category = () => {
     return (
         <div className="mx-5">
-            <span className=" flex justify-center items-center capitalize text-4xl font-serif  font-medium bg-clip-text text-transparent bg-gradient-to-r from-amber-600/50 to-amber-700">shop by category</span>
+            <span className="flex justify-center items-center capitalize text-4xl font-serif  font-medium bg-clip-text text-transparent bg-gradient-to-r from-amber-600/50 to-amber-700">shop by category</span>
             <div className="w-full mt-10 ">
                 < p className="flex items-center justify-center">
-                    <motion.span whileHover={{ scale: 1.2 }}  className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer">
+                    <motion.span whileHover={{ scale: 1.2 }}  className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">
                     natural makeup</motion.span>
-                    <motion.span  whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer">skincare </motion.span>
-                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer">bath & body</motion.span>
+                    <motion.span  whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">skincare </motion.span>
+                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">bath & body</motion.span>
                 </p>
                 <p className="flex items-center justify-center">
-                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer">haircare</motion.span>
-                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer">pure fragrances </motion.span>
-                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer">skin</motion.span>
-                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer">hair</motion.span>
+                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">haircare</motion.span>
+                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">pure fragrances </motion.span>
+                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">skin</motion.span>
+                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">hair</motion.span>
                 </p>
                 <p className="flex items-center justify-center">
-                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer">combo box</motion.span>
-                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer">mega sale</motion.span>
+                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">combo box</motion.span>
+                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">mega sale</motion.span>
                 </p>
             </div>
+
         </div>
     )
 }
