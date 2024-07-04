12a135ed21f9fc2372e8fe307a5a03444b61b076
Amitsaini-tech <sainiamit3464@gmail.com>
2024-07-04 17:20:00 +0530

    mid2container link

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Homecontainer from './Homecontainer'
 import Category from './category'
+import Mid2Container from './Mid2Container'
+
 
 const MainContainer = () => {
   return (
     <div className="">
       <Homecontainer/>
       <Category/>
+      <Mid2Container/>
     </div>
   )
 }
