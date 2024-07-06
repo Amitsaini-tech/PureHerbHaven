c392bc59b4a678642fbac409b4f69a0ab2b97d81
Amitsaini-tech <sainiamit3464@gmail.com>
2024-07-06 17:05:54 +0530

    link the new page

--- src/Components/category.jsx
+++ src/Components/category.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from "framer-motion"
+import { Link } from 'react-router-dom'
 
 const Category = () => {
     return (
@@ -7,8 +8,9 @@ const Category = () => {
             <span className="flex justify-center items-center capitalize text-4xl font-serif  font-medium bg-clip-text text-transparent bg-gradient-to-r from-amber-600/50 to-amber-700">shop by category</span>
             <div className="w-full m-10 ">
                 < p className="flex items-center justify-center">
-                    <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">
-                        natural makeup</motion.span>
+                <Link to={"/Naturalb"}>
+                    <motion.span whileHover={{ scale: 1.2 }} id='Naturally' className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">
+                        natural makeup</motion.span></Link>
                     <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">skincare </motion.span>
                     <motion.span whileHover={{ scale: 1.2 }} className=" flex items-center justify-center w-[15rem] h-10 bg-gray-50/30 shadow-lg drop-shadow-md  rounded-xl m-5 text-lg capitalize font-sans font-medium cursor-pointer hover:bg-rose-400 hover:text-gray-100">bath & body</motion.span>
                 </p>
