e9c3c572986e542fe679ef33b711950e38f0f992
Amitsaini-tech <sainiamit3464@gmail.com>
2024-07-06 17:05:14 +0530

    add or link the new file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import MainContainer from "./Components/MainContainer"
 import { Route, Routes } from 'react-router-dom'
 import Header from './Components/Header'
+import Naturalb from './Components/Naturalb'
 const App = () => {
   return (
     <div className="w-screen h-full flex flex-col">
@@ -9,6 +10,7 @@ const App = () => {
       <main className=" h-full mt-8 md:mt-12 lg:mt-1 w-full">
         <Routes>
           <Route path='/*' element={<MainContainer />} />
+          <Route path='/Naturalb' element={<Naturalb/>}/>
         </Routes>
       </main>
     </div>
